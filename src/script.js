35afe1d9837bca643544f071943d68f6663970de
Stino01 <agostino.carrieri01@gmail.com>
2022-03-28 16:18:25 +0200

    [index and script] milestone2

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
-let num = 100;
+const app = document.getElementById('app')
+const row = document.createElement("div");
+row.setAttribute('class', 'row g-3');
+
+const num = 100;
 for (let i = 1; i <= num; i++) {
+    const col = document.createElement('div');
+    col.setAttribute('class', 'col-6 col-sm-6 col-md-3 col-lg-2');
+    const box = document.createElement('div');
+    box.setAttribute('class', 'square');
+
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log('FizzBuzz');
+        box.innerHTML = 'FizzBuzz';
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+        box.innerHTML += 'Fizz';
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        box.innerHTML += 'Buzz';
     } else {
-        console.log(i);
+        box.innerHTML += i;
     }
+    col.append(box);
+    row.append(col)
 }
+app.append(row) 
